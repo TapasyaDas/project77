b5c91b40822af4685c9bd056ca5f4dde89b8b3e7
TapasyaDas <131509265+TapasyaDas@users.noreply.github.com>
2023-08-29 17:36:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+console.log("Hi,I am Console Screen");
+    var first_name = "Ranbir";
+console.log(first_name);
+
+var last_name = "Kapoor";
+console.log(last_name);
+
+var full_name = first_name.concat(last_name);
+console.log(full_name);
+var a= 10;
+var b= 5;
+var add= a+b;
+console.log(add);
+var sub= a-b;
+console.log(sub);
+var multiply= a*b;
+console.log(multiply);
+var divide= a/b;
+console.log(divide);
